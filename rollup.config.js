e05e1cbc5090fcae0125f9c46a5a848385b393d9
Aaron Long <aaron.long.c@gmail.com>
2023-08-05 12:13:57 -0400

    build(rollup): Replace plugins for standard

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,37 @@
 import esbuild from "rollup-plugin-esbuild";
 import { dts } from "rollup-plugin-dts";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: "src/index.ts",
+  // https://rollupjs.org/configuration-options/#input
+  // https://rollupjs.org/configuration-options/#output-extend
+  // https://github.com/rollup/rollup/blob/651e49353eb98bf66e0efd7b27174591a4557880/src/rollup/types.d.ts#L838-L847
+  input: ["src/index.ts", "src/BarcodeDetectorZXing.ts"],
   output: [
     {
-      file: "dist/index.cjs",
+      dir: "dist",
       format: "cjs",
-      sourcemap: true,
+      // sourcemap: true,
+      entryFileNames: (chunk) => {
+        return `${chunk.name}.cjs`;
+      },
     },
     {
-      file: "dist/index.mjs",
+      dir: "dist",
       format: "es",
-      sourcemap: true,
+      // sourcemap: true,
+      entryFileNames: (chunk) => {
+        return `${chunk.name}.mjs`;
+      },
     },
-    { file: "dist/index.d.ts", format: "es" },
+    // {
+    //   dir: "dist",
+    //   // entryFileNames: (chunk) => {
+    //   //   return `${chunk.name}.d.ts`;
+    //   // },
+    //   format: "es",
+    // },
   ],
-  plugins: [esbuild(), dts()],
+  plugins: [typescript()],
   external: ["@zxing/library", "@zxing/browser"],
 };
